20f010fa84f5991c73fecc0104009c2fd8dfe2e6
mariaoliv <77213389+mariaoliv@users.noreply.github.com>
2023-10-15 17:12:54 -0400

    stuff

--- applicationInterface/authenticationService.py
+++ applicationInterface/authenticationService.py
@@ -7,8 +7,10 @@ from wsgiref.simple_server import make_server
 conn = psycopg2.connect(
     host="localhost",
     database="commons",
-    user="postgres",
-    password="root"
+    #user="postgres",
+    #password="root"
+    user="commons_dev",
+    password="commons_dev"
 )
 """
 user="commons_dev",
@@ -19,15 +21,15 @@ password="commons_dev"
 cur = conn.cursor()
 
 # Verify if an account already exists.
-def verifyAccount(username,password):
+def verifyAccount(email,password):
     # Hash the password we receive.
-    hashedPassword = password # Test
-    #hashedPassword = bcrypt.hashpw(password.encode('utf8'),bcrypt.gensalt())
+    #hashedPassword = password # Test
+    hashedPassword = bcrypt.hashpw(password.encode('utf8'),bcrypt.gensalt())
 
     # Check if we find a username and password that matches.
     try:
-        cur.execute("SELECT * FROM test WHERE id=%s and number=%s",(username,hashedPassword)) # Test
-        #cur.execute("SELECT * FROM Students WHERE username=%s and password=%s",(username,hashedPassword))
+        #cur.execute("SELECT * FROM test WHERE id=%s and number=%s",(username,hashedPassword)) # Test
+        cur.execute("SELECT * FROM student WHERE email=%s and password=%s",(email,hashedPassword))
         result = cur.fetchall()
     except:
         return False
@@ -39,11 +41,11 @@ def verifyAccount(username,password):
     return False
 
 # Register a new account.
-def registerAccount(username,password):
+def registerAccount(email,password):
     # Check if the username exists, if it does return false.
     try:
-        cur.execute("SELECT * FROM test WHERE id=%s",(username)) # Test
-        #cur.execute("SELECT * FROM Students WHERE username=%s",(username))
+        #cur.execute("SELECT * FROM test WHERE id=%s",(username)) # Test
+        cur.execute("SELECT * FROM Students WHERE email=%s",(email))
         result = cur.fetchall()
     except:
         return False
@@ -53,8 +55,8 @@ def registerAccount(username,password):
     hashedPassword = password # Test
     #hashedPassword = bcrypt.hashpw(password.encode('utf8'),bcrypt.gensalt())
     try:
-        cur.execute("INSERT INTO test VALUES (%s,%s)",(username,hashedPassword)) # Test
-        #cur.execute("INSERT INTO Students VALUES (%s,%s)",(username,hashedPassword))
+        #cur.execute("INSERT INTO test VALUES (%s,%s)",(username,hashedPassword)) # Test
+        cur.execute("INSERT INTO Students VALUES (%s,%s)",(email,hashedPassword))
     except:
         return False
     return True
@@ -64,13 +66,13 @@ class RootController(TGController):
 
     # Method to handle user authentication requests.
     @expose('json')
-    def authenticateUserSignIn(self, username, password):
-        return {"result":verifyAccount(username,password)}
+    def authenticateUserSignIn(self, email, password):
+        return {"result":verifyAccount(email,password)}
 
     # Method to handle new user registration.
     @expose('json')
-    def registerNewUser(self, username, password):
-        return {"result":registerAccount(username,password)}
+    def registerNewUser(self, email, password):
+        return {"result":registerAccount(email,password)}
     
 config = AppConfig(minimal = True, root_controller = RootController())
 application = config.make_wsgi_app()
