03097038a37a36225350aeb4fdf9627adb83fff3
jbekkk <jb7478@nyu.edu>
2023-10-15 23:40:13 -0400

    working chatroom updates

---
+++ ui/.next/server/vendor-chunks/react-chat-elements.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "vendor-chunks/react-chat-elements";
+exports.ids = ["vendor-chunks/react-chat-elements"];
+exports.modules = {
+
+/***/ "(ssr)/./node_modules/react-chat-elements/dist/main.css":
+/*!********************************************************!*\
+  !*** ./node_modules/react-chat-elements/dist/main.css ***!
+  \********************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (\"49e4864e0719\");\nif (false) {}\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKHNzcikvLi9ub2RlX21vZHVsZXMvcmVhY3QtY2hhdC1lbGVtZW50cy9kaXN0L21haW4uY3NzIiwibWFwcGluZ3MiOiI7Ozs7QUFBQSxpRUFBZSxjQUFjO0FBQzdCLElBQUksS0FBVSxFQUFFLEVBQXVCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vY29tbW9ucy8uL25vZGVfbW9kdWxlcy9yZWFjdC1jaGF0LWVsZW1lbnRzL2Rpc3QvbWFpbi5jc3M/MTU4OCJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCBcIjQ5ZTQ4NjRlMDcxOVwiXG5pZiAobW9kdWxlLmhvdCkgeyBtb2R1bGUuaG90LmFjY2VwdCgpIH1cbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///(ssr)/./node_modules/react-chat-elements/dist/main.css\n");
+
+/***/ })
+
+};
+;
---
+++ ui/src/app/chatroom/page.js
@@ -0,0 +1,23 @@
+"use client"
+
+import Home from '../components/Home';
+import ChatPage from '../components/ChatPage';
+import Link from "next/link";
+
+
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/chatt">Back to home</Link>
+      </h2>
+    </>
+  );
+}
+
+
+
+
+
