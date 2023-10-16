a2bd5636ef083eea7c16136fdff6a3a340b1bae5
mariaoliv <77213389+mariaoliv@users.noreply.github.com>
2023-10-15 21:44:40 -0400

    fixed database and server.py

--- applicationInterface/server.py
+++ applicationInterface/server.py
@@ -1,5 +1,6 @@
 import psycopg2
 import bcrypt
+import json
 from tg import expose, TGController, AppConfig
 from wsgiref.simple_server import make_server
 
@@ -24,12 +25,13 @@ cur = conn.cursor()
 def verifyAccount(email,password):
     # Hash the password we receive.
     #hashedPassword = password # Test
-    hashedPassword = bcrypt.hashpw(password.encode('utf8'),bcrypt.gensalt())
+    salt = bcrypt.gensalt()
+    hashedPassword = bcrypt.hashpw(password.encode('utf8'), salt)
 
     # Check if we find a username and password that matches.
     try:
         #cur.execute("SELECT * FROM test WHERE id=%s and number=%s",(username,hashedPassword)) # Test
-        cur.execute("SELECT * FROM student WHERE email=%s and password=%s",(email,hashedPassword))
+        cur.execute(f"SELECT * FROM student WHERE email={email} and password={hashedPassword}")
         result = cur.fetchall()
     except:
         return False
@@ -41,36 +43,34 @@ def verifyAccount(email,password):
     return False
 
 # Register a new account.
-def registerAccount(email,password):
+def registerAccount(email, password):
     # Check if the username exists, if it does return false.
-    try:
-        #cur.execute("SELECT * FROM test WHERE id=%s",(username)) # Test
-        cur.execute("SELECT * FROM Students WHERE email=%s",(email))
-        result = cur.fetchall()
-    except:
-        return False
+    cur.execute(f"SELECT * FROM Student WHERE email='{email}'")
+    result = cur.fetchall()
+
     if len(result) != 0:
         return False
-    
-    hashedPassword = password # Test
-    #hashedPassword = bcrypt.hashpw(password.encode('utf8'),bcrypt.gensalt())
+
+    salt = bcrypt.gensalt()
+    hashedPassword = bcrypt.hashpw(password.encode('utf8'), salt)
     try:
-        #cur.execute("INSERT INTO test VALUES (%s,%s)",(username,hashedPassword)) # Test
-        cur.execute("INSERT INTO Students VALUES (%s,%s)",(email,hashedPassword))
+        # cur.execute("INSERT INTO test VALUES (%s,%s)",(username,hashedPassword)) # Test
+        cur.execute(f"INSERT INTO Student VALUES ('{email}','{hashedPassword}')")
     except:
         return False
     return True
 
 def createProfile(student_id, uni_id, name, graduation_year, major, hobbies, interests):
     try:
-        cur.execute('INSERT INTO student_profile VALUES(%s, %s, %s, %s, %s, %s, %s)', (student_id, uni_id, name, graduation_year, major, hobbies, interests))
+        cur.execute(f"INSERT INTO student_profile VALUES('{student_id}', '{uni_id}', '{name}', '{graduation_year}', '{major}', '{hobbies}', '{interests}')")
     except:
         return False
     return True
 
 def retrieveProfileData(student_id, uni_id):
-    cur.execute('SELECT * FROM student_profile WHERE student_id = %s AND uni_id = %s', (student_id, uni_id))
+    cur.execute(f"SELECT * FROM student_profile WHERE student_id = '{student_id}' AND uni_id = '{uni_id}'")
     result = cur.fetchall()
+    result = json.dumps(result)
     return result
 
 
