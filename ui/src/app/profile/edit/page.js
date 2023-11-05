150819d56c90f70aae418401811fea51c78631ce
allison-lee6444 <al6897@nyu.edu>
2023-11-05 03:01:36 -0500

    solved csrf and sqli vulnerabilities

--- ui/src/app/profile/edit/page.js
+++ ui/src/app/profile/edit/page.js
@@ -21,6 +21,17 @@ function EditProfile() {
   });
 
   let data;
+  const sessionid = getCookie('sessionid');
+  if (sessionid === null) {
+    return (
+      <html>
+      <body>
+      <p>Hey! You are not logged in. You will be redirected to the login page.</p>
+      {window.location.replace('/login')}
+      </body>
+      </html>
+    )
+  }
 
   // Generate JSX code for error message
   const renderErrorMessage = (name) =>
@@ -53,24 +64,24 @@ function EditProfile() {
     const new_interests = document.forms[0][8]._valueTracker.getValue();
     const current_pw = document.forms[0][9]._valueTracker.getValue();
     const new_pw = document.forms[0][10]._valueTracker.getValue();
-    fname=new_fname===''?fname:new_fname;
-    lname=new_lname===''?lname:new_lname;
-    new_email=new_email===''?email:new_email;
-    hobbies=new_hobbies===''?hobbies:new_hobbies;
-    interests=new_interests===''?interests:new_interests;
+    fname = new_fname === '' ? fname : new_fname;
+    lname = new_lname === '' ? lname : new_lname;
+    new_email = new_email === '' ? email : new_email;
+    hobbies = new_hobbies === '' ? hobbies : new_hobbies;
+    interests = new_interests === '' ? interests : new_interests;
 
     set_profile_info({student_id, uni_id, email, graduation_year, major, hobbies, interests, fname, lname});
 
     // update user profile
     const postProfileData = async () => {
       try {
-        const response = await fetch('http://127.0.0.1:8060/editStudentProfile/' + email + '?hobbies=' + hobbies + '&interests=' + interests + '&fname=' + fname + '&lname=' + lname + '&new_email=' + new_email);
+        const response = await fetch('http://127.0.0.1:8060/editStudentProfile/?sessionid=' + sessionid + '?hobbies=' + encodeURIComponent(hobbies) + '&interests=' + encodeURIComponent(interests) + '&fname=' + encodeURIComponent(fname) + '&lname=' + encodeURIComponent(lname) + '&new_email=' + encodeURIComponent(new_email), {method: "POST"});
         data = await response.json();
       } catch (error) {
         setErrorMessages({name: "server", message: "Server Error: " + error})
       }
     }
-    postProfileData().then(()=>window.location.replace('/profile'));
+    postProfileData().then(() => window.location.replace('/profile'));
 
   };
 
@@ -82,10 +93,15 @@ function EditProfile() {
 
 
   const fetchData = async () => {
-    if(received_reply){return;}
+    if (received_reply) {
+      return;
+    }
     try {
-      const cookie_email = getCookie('email');
-      const response = await fetch('http://127.0.0.1:8060/getStudentProfileData/' + cookie_email);
+      const sessionid = getCookie('sessionid');
+      if (sessionid === null) {
+        window.location.replace('/login');
+      }
+      const response = await fetch('http://127.0.0.1:8060/getStudentProfileData/' + sessionid);
       data = await response.json();
     } catch (error) {
       setErrorMessages({name: "server", message: "Server Error: " + error})
@@ -93,7 +109,9 @@ function EditProfile() {
   }
 
   fetchData().then(() => {
-    if (received_reply) {return;}
+    if (received_reply) {
+      return;
+    }
     if (data.length === 0) {
       data.push(['', '', getCookie('email'), '', '', '', '', '', '']);
     }
@@ -110,7 +128,6 @@ function EditProfile() {
         return prevState;
       });
   }
-
 
   // load when api reply received and variables populated
   return (received_reply && (
@@ -168,7 +185,7 @@ function EditProfile() {
                 <div className="form-group">
                   <label className="col-sm-2 control-label">Email</label>
                   <div className="col-sm-10">
-                    <input type="email" className="form-control" placeholder={profile_info.email} disabled={false} />
+                    <input type="email" className="form-control" placeholder={profile_info.email} disabled={false}/>
                   </div>
                 </div>
               </div>
