6a8a5b79b5b18181694aabbf2d2f0a3c6cac326e
allison-lee6444 <al6897@nyu.edu>
2023-11-08 21:26:40 -0500

    fixed request method and arguments. changed makefile run-server command

--- applicationInterface/server.py
+++ applicationInterface/server.py
@@ -16,73 +16,85 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+
 # Method to handle user authentication requests.
 @app.get("/authenticateUserSignIn")
-def authenticateUserSignIn(self, email, password):
+def authenticateUserSignIn(email, password):
     return {"result": verifyAccount(email, password)}
 
+
 # Method to handle new user registration.
-@app.get("/registerNewUser")
-def registerNewUser(self, email, password):
+@app.post("/registerNewUser")
+def registerNewUser(email, password):
     return {"result": registerAccount(email, password)}
 
+
 # Method to create student profile
-@app.get("/editStudentProfile")
-def editStudentProfile(self,email, hobbies, interests, fname, lname,new_email):
-    print(email, hobbies, interests, fname, lname,new_email)
+@app.post("/editStudentProfile")
+def editStudentProfile(email, hobbies, interests, fname, lname, new_email):
     return {
-        "result": editProfile(email,hobbies, interests, fname, lname,new_email)}
+        "result": editProfile(email, hobbies, interests, fname, lname, new_email)}
+
 
 # Method to retrieve data of a student profile for a particular student
 @app.get("/getStudentProfileData")
-def getStudentProfileData(self, email):
+def getStudentProfileData(email):
     return retrieveProfileData(email)
+
 
 # Method used by the front-end to check if there are any new messages.
 @app.get("/newMesages")
-def newMesages(self,chatroomID,dateTime):
-    return checkForMessages(chatroomID,dateTime)
+def newMesages(chatroomID, dateTime):
+    return checkForMessages(chatroomID, dateTime)
+
 
 # Method to create a new event and save it in the DB.
-@app.get("/scheduleNewEvent")
-def scheduleNewEvent(self,eventName,hostID,description,locName,locCoord,startTime,endTime,eventID):
-    return {"success":createEvent(eventName,hostID,description,locName,locCoord,startTime,endTime,eventID)}
+@app.post("/scheduleNewEvent")
+def scheduleNewEvent(eventName, hostID, description, locName, locCoord, startTime, endTime, eventID):
+    return {"result": createEvent(eventName, hostID, description, locName, locCoord, startTime, endTime, eventID)}
+
 
 # Method called when a student joins a event.
-@app.get("/joinEventClicked")
-def joinEventClicked(self,eventID,studentID,chatroomID):
-    return {"success":studentJoinEvent(eventID,studentID,chatroomID)}
+@app.post("/joinEventClicked")
+def joinEventClicked(eventID, studentID, chatroomID):
+    return {"result": studentJoinEvent(eventID, studentID, chatroomID)}
 
-# Method called when a student leaves a event.
-@app.get("/leaveEventClicked")
-def leaveEventClicked(self,studentID,eventID):
-    return {"success":deleteEvent(studentID,eventID)}
+
+# Method called when a student leaves an event.
+@app.post("/leaveEventClicked")
+def leaveEventClicked(studentID, eventID):
+    return {"result": deleteEvent(studentID, eventID)}
+
 
 # Method called when the host of an event cancels the event.
-@app.get("/cancelScheduledEvent")
-def cancelScheduledEvent(self,hostID,eventID):
-    return {"success":cancelEvent(hostID,eventID)}
+@app.post("/cancelScheduledEvent")
+def cancelScheduledEvent(hostID, eventID):
+    return {"result": cancelEvent(hostID, eventID)}
 
-# Method called to get all events a student is apart of.
+
+# Method called to get all events a student is a part of.
 @app.get("/getUserEvents")
-def getUserEvents(self,studentID):
+def getUserEvents(studentID):
     result = getUserEvents(studentID)
     if not result:
-        return {"success":False}
-    return {"events":result}
+        return {"result": False}
+    return {"result": result}
+
 
 # Method called to get all courses a student is in.
 @app.get("/getUserCourses")
-def getUserCourses(self,studentID):
+def getUserCourses(studentID):
     result = getUserCourses(studentID)
     if not result:
-        return {"success":False}
-    return {"courses":result}
+        return {"success": False}
+    return {"courses": result}
+
 
 # Method called to help a student identify if joining an event will create a time conflict.
 @app.get("/identifyTimeConflict")
-def identifyTimeConflict(self,startTime,endTime,studentID):
-    return {"timeConflictExists":identifiedTimeConflict(startTime,endTime,studentID)}
+def identifyTimeConflict(startTime, endTime, studentID):
+    return {"timeConflictExists": identifiedTimeConflict(startTime, endTime, studentID)}
+
 
 # <<< [TEST - DELETE AFTER TEST] >>> #
 """@app.get("/test")
