38770ac4445c37cd5f2e952543352d5ef39474ae
mariaoliv <77213389+mariaoliv@users.noreply.github.com>
2023-11-09 16:01:54 -0500

    Update create_tables.sql

--- university_server/create_tables.sql
+++ university_server/create_tables.sql
@@ -8,21 +8,24 @@ DROP TABLE IF EXISTS course;
 CREATE TABLE IF NOT EXISTS student (
     email varchar(255) NOT NULL,
     student_id bigint NOT NULL,
+    uni_id varchar(255) NOT NULL,
     fname varchar(255),
     lname varchar(255),
     major varchar(255),
     graduation_year int,
-    primary key(email, student_id)
+    primary key(email, student_id, uni_id)
 );
 
 CREATE TABLE IF NOT EXISTS course (
     id varchar(255) NOT NULL,
-    primary key(id)
+    uni_id varchar(255) NOT NULL,
+    primary key(id, uni_id)
 );
 
 CREATE TABLE IF NOT EXISTS section (
     course_id varchar(255) NOT NULL,
     section_id varchar(255) NOT NULL,
+    uni_id varchar(255) NOT NULL,
     start_time time,
     end_time time,
     semStartDate date,
@@ -35,16 +38,17 @@ CREATE TABLE IF NOT EXISTS section (
     meetsFri boolean,
     meetsSat boolean,
     meetsSun boolean,
-    primary key(course_id, section_id),
-    foreign key (course_id) REFERENCES course(id) ON DELETE CASCADE
+    primary key(course_id, section_id, uni_id),
+    foreign key (course_id, uni_id) REFERENCES course(id, uni_id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS takes (
     student_id bigint NOT NULL,
     course_id varchar(255) NOT NULL,
     section_id varchar(255) NOT NULL,
-    primary key(student_id, course_id),
-    foreign key(course_id, section_id) REFERENCES section(id, section_id) ON DELETE CASCADE
+    uni_id varchar(255) NOT NULL,
+    primary key(student_id, course_id, uni_id),
+    foreign key(course_id, section_id, uni_id) REFERENCES section(id, section_id, uni_id) ON DELETE CASCADE
 );
 
 COMMIT;
