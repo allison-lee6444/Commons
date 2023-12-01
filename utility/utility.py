202e83c9aff457a57036da9795c896518d68f960
erw9695 <erw9695@nyu.edu>
2023-12-01 12:31:47 -0500

    Wrote command line utility to automatically remove old courses and import schedule again.

--- utility/utility.py
+++ utility/utility.py
@@ -1,13 +1,28 @@
 import datetime
 from ..server.cursor import cur
+from ..server.schedule import request_schedule
 
 # Go through the takes table, identify which courses are old based on semester end date, and delete them from takes.
 def del_old_courses():
-    cur.execute("SELECT * FROM takes")
+    currentDate = datetime.datetime.now().strftime('%Y-%m-%d')
+    cur.execute(
+        "DELETE FROM takes WHERE (SELECT section.semEndDate FROM section WHERE section.course_id = takes.course_id"
+        " AND section.section_id = takes.section_id) < %(currentDate)s",{'currentDate':currentDate}
+    )
 
-# Ask university_server for the student's schedule, if the current date is during the semester, add it to takes.
+# Ask university_server for the student's schedule (only if they are verified), if the current date is during the semester, add it to takes.
 def add_new_courses():
-    pass
+    # Get a list of student emails that have been verified (and had their schedule imported already).
+    cur.execute("SELECT email FROM student WHERE student_id IS NOT NULL;")
+    result = cur.fetchall()
+    emails = [i[0] for i in result]
+
+    # Go through email and call the same function used to import their schedule when they first register.
+    # Assumption: University server will have the current schedule for the student.
+    for email in emails:
+        status = request_schedule(cur,email)
+        if not status:
+            print("Error importing schedule for student with email: "+email)
 
 del_old_courses()
 add_new_courses()
