a020f8dba865bad3918504fcb660eab2d57e4a2b
erw9695 <erw9695@nyu.edu>
2023-12-01 12:51:16 -0500

    Finished cl utility.

--- server/schedule.py
+++ server/schedule.py
@@ -9,9 +9,13 @@ def request_schedule(cur,email):
 
         try:
             for row in takesData:
-                # Order is different in tables.
-                value = (row[0],row[3],row[1],row[2]) 
-                cur.execute("INSERT INTO takes VALUES %(value)s",{'value':value})
+                # Make sure this course/section isn't already in takes.
+                cur.execute("SELECT * FROM takes WHERE course_id=%(course)s AND section_id=%(sec)s",{'course':row[1],'sec':row[2]})
+                result = cur.fetchall()
+                if len(result) == 0:
+                    # Order is different in tables.
+                    value = (row[0],row[3],row[1],row[2]) 
+                    cur.execute("INSERT INTO takes VALUES %(value)s",{'value':value})
             cur.execute("COMMIT")
         except Exception as e:
             print(e)
--- server/staff_utility.py
+++ server/staff_utility.py
@@ -1,17 +1,20 @@
 import datetime
-from ..server.cursor import cur
-from ..server.schedule import request_schedule
+from cursor import cur
+from schedule import request_schedule
 
 # Go through the takes table, identify which courses are old based on semester end date, and delete them from takes.
 def del_old_courses():
+    print("Starting schedule deletion.")
     currentDate = datetime.datetime.now().strftime('%Y-%m-%d')
     cur.execute(
         "DELETE FROM takes WHERE (SELECT section.semEndDate FROM section WHERE section.course_id = takes.course_id"
         " AND section.section_id = takes.section_id) < %(currentDate)s",{'currentDate':currentDate}
     )
+    print("Schedule deletion complete.")
 
 # Ask university_server for the student's schedule (only if they are verified), if the current date is during the semester, add it to takes.
 def add_new_courses():
+    print("Starting schedule import.")
     # Get a list of student emails that have been verified (and had their schedule imported already).
     cur.execute("SELECT email FROM student WHERE student_id IS NOT NULL;")
     result = cur.fetchall()
@@ -23,6 +26,8 @@ def add_new_courses():
         status = request_schedule(cur,email)
         if not status:
             print("Error importing schedule for student with email: "+email)
+    print("Schedule import complete.")
 
 del_old_courses()
 add_new_courses()
+print("Utility complete.")
