e4d9e7b349ae13cbf12b5f6004b69c4c2bbaab28
Allison Lee <al6897@nyu.edu>
2023-12-06 20:39:00 -0500

    added link to events and profile button in chat (#37)

--- ui/src/app/components/ChatBar.js
+++ ui/src/app/components/ChatBar.js
@@ -11,14 +11,22 @@ const ChatBar = ({socket}) => {
     socket.on("newUserResponse", data => setUsers(data))
   }, [socket, users])
 
+  const handleEventsClick = () => {
+    window.location.replace('/events');
+  }
+
+  const handleProfileClick = () => {
+    window.location.replace('/profile');
+  }
+
 
   return (
     <div className='chat__sidebar'>
       <h2 className="commons-logo">Commons</h2>
       <div>
         <h4 className='chat__header'>MENU</h4>
-        <button type='button' className='menu_button'>Events</button>
-        <button type='button' className='menu_button'>Profile</button>
+        <button type='button' className='menu_button' onClick={handleEventsClick}>Your Events</button>
+        <button type='button' className='menu_button' onClick={handleProfileClick}>Profile</button>
       </div>
       <div>
         <h4 className='chat__header'>CHATROOMS</h4>
