a7ac1319f2338e47c71f9253fc1c01f57b7ecfc9
erw9695 <erw9695@nyu.edu>
2023-12-06 19:47:33 -0500

    Fixed dynamic routes.

--- ui/src/app/event/edit/[event_id].js
+++
@@ -1,9 +0,0 @@
-import { useRouter } from 'next/router';
-
-export default function Page() {
-    const router = useRouter();
-    const event_id = router.query.event_id;
-
-    // Testing only.
-    return <p>The event id is: {event_id}</p>
-};
